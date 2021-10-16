c3285a38d6aead3a718552a4ceafffcffe8261b8
mrisqiamiruladieb <92592932+mrisqiamiruladieb@users.noreply.github.com>
2021-10-16 20:33:12 +0700

    Update search_login.php

--- search_login.php
+++ search_login.php
@@ -7,12 +7,12 @@
   
   $qpost=mysqli_query($db,"SELECT * FROM post ORDER BY tgl_insert DESC LIMIT 5")or die(mysqli_error());
   $rpost=mysqli_fetch_array($qpost);	
-	$search=$_GET['search'];
+	$search=filter_input(INPUT_GET, 'search');
 	$query = "SELECT * FROM post WHERE judul LIKE '%$search%' OR isipost LIKE '%$search%'";
 	$query_search = mysqli_query($db, $query);
 
 	if(!$query_search){
-		die("QUERY FAILED" . mysqli_error($db));
+		("QUERY FAILED" . mysqli_error($db));
 	}
 
 	$count = mysqli_num_rows($query_search);
@@ -57,11 +57,11 @@
 					return '<div class="post-meta">';
 					return '<span class="d-block">';
 					$idpenulis = $idpenulis;
-					$qpenulis=mysqli_query($db,"SELECT * FROM penulis where idpenulis='$idpenulis'")or die(mysqli_error());
+					$qpenulis=mysqli_query($db,"SELECT * FROM penulis where idpenulis='$idpenulis'")or (mysqli_error());
 					$rpenulis=mysqli_fetch_array($qpenulis);
 					return $rpenulis['nama'];
 					$idkategori = $idkategori;
-					$qkategori=mysqli_query($db,"SELECT * FROM kategori where idkategori='$idkategori'")or die(mysqli_error());
+					$qkategori=mysqli_query($db,"SELECT * FROM kategori where idkategori='$idkategori'")or (mysqli_error());
 					$rkategori=mysqli_fetch_array($qkategori);
 					return ' in <a href="categories_login.php?id='.$rkategori['idkategori'].'">'.$rkategori['nama'].'</a></span>';
 					return '<span class="date-read">';
