80f9bf2a04dce5c503c407ef22ffa40bd6c85339
atiqohh <76618855+atiqohh@users.noreply.github.com>
2021-10-16 20:16:52 +0700

    update categories_login.php

--- categories_login.php
+++ categories_login.php
@@ -1,11 +1,11 @@
 <?php
   include 'db_login.php';
   session_start();
-  $user=$_SESSION['username'];
+  $user=filter_input(INPUT_SESSION, 'username');
   $query=mysqli_query($db,"SELECT * FROM penulis where email='$user'")or die(mysqli_error());
   $row=mysqli_fetch_array($query);
   
-  $idkategori=$_GET['id'];
+  $idkategori=filter_input(INPUT_GET, 'id');
   $qpost=mysqli_query($db,"SELECT * FROM post ORDER BY tgl_insert DESC LIMIT 5")or die(mysqli_error());
   $rpost=mysqli_fetch_array($qpost);	
 ?>
