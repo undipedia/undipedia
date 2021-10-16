8d67f3cbd7620ac28586337f9f2e85d0b867c68b
mrisqiamiruladieb <92592932+mrisqiamiruladieb@users.noreply.github.com>
2021-10-16 16:47:49 +0700

    Update search.php

--- search.php
+++ search.php
@@ -26,7 +26,7 @@
             <div class="section-title" style="margin-bottom:30px">
               <h2>Hasil Pencarian</h2>
             </div>
-			<div class="error"><?php if (isset($error_search)) echo $error_search;?></div>
+			<div class="error"><?php if (isset($error_search)) return $error_search;?></div>
 			<?php  	
 				while($data = mysqli_fetch_assoc($query_search)){
 					$idpost = $data['idpost'];
@@ -36,11 +36,11 @@
 					$file_gambar = $data['file_gambar'];
 					$tgl_insert = $data['tgl_insert'];
 					$idpenulis = $data['idpenulis'];
-					echo '<div class="post-entry-2 d-flex">';
-					echo '<div class="thumbnail order-md-2" style="background-image: url(data:image/jpeg;base64,'.base64_encode($file_gambar).')"></div>';
-					echo '<div class="contents order-md-1 pl-0">';
-					echo '<h2><a href="show_post_pengunjung.php?id='.$idpost.'">'.$judul.'</a></h2>';
-						echo '<p>';
+					return '<div class="post-entry-2 d-flex">';
+					return '<div class="thumbnail order-md-2" style="background-image: url(data:image/jpeg;base64,'.base64_encode($file_gambar).')"></div>';
+					return '<div class="contents order-md-1 pl-0">';
+					return '<h2><a href="show_post_pengunjung.php?id='.$idpost.'">'.$judul.'</a></h2>';
+						return '<p>';
 						$text=$isipost;
 						$num_char=255;
 						$cut_text = substr($text, 0, $num_char);
@@ -48,23 +48,23 @@
 							$new_pos = strrpos($cut_text, ' ');
 							$cut_text = substr($text, 0, $new_pos);
 						}
-						echo $cut_text . '...';
-						echo '</p>';
-					echo '<div class="post-meta">';
-					echo '<span class="d-block">';
+						return $cut_text . '...';
+						return '</p>';
+					return '<div class="post-meta">';
+					return '<span class="d-block">';
 					$idpenulis = $idpenulis;
 					$qpenulis=mysqli_query($db,"SELECT * FROM penulis where idpenulis='$idpenulis'")or die(mysqli_error());
 					$rpenulis=mysqli_fetch_array($qpenulis);
-					echo $rpenulis['nama'];
+					return $rpenulis['nama'];
 					$idkategori = $idkategori;
 					$qkategori=mysqli_query($db,"SELECT * FROM kategori where idkategori='$idkategori'")or die(mysqli_error());
 					$rkategori=mysqli_fetch_array($qkategori);
-					echo ' in <a href="categories.php?id='.$rkategori['idkategori'].'">'.$rkategori['nama'].'</a></span>';
-					echo '<span class="date-read">';
+					return ' in <a href="categories.php?id='.$rkategori['idkategori'].'">'.$rkategori['nama'].'</a></span>';
+					return '<span class="date-read">';
 					$date = new DateTime($tgl_insert);
-					echo $date->format('M d, Y');
-					echo '<span class="mx-1">&bullet;</span> <span class="icon-star2"></span></span>';
-					echo '</div></div></div>';
+					return $date->format('M d, Y');
+					return '<span class="mx-1">&bullet;</span> <span class="icon-star2"></span></span>';
+					return '</div></div></div>';
 				}
 			?>
       </div>
@@ -75,7 +75,7 @@
             <form action="#" class="row align-items-center">
             <div class="col-md-10 mr-auto" style="text-align: justify;">
                 <h2>UNDIPedia</h2>
-                <p>UDIPedia adalah sebuah blog dimana kamu dapat mencari dan saling berbagi mengenai informasi unik, terbaru, terhangat dan juga informatif mengenai semua hal yang terjadi di sekitar Undip. Banyak artikel-artikel yang membantu dan juga bermanfaat, mulai dari tips bagi mahasiswa yang merantau, tips olahraga, mengenal lingkungan di sekitar Undip, serta mencari informasi mengenai lomba. Kamu dapat menjadi pengunjung, maupun penulis dalam blog ini. Tunggu apalagi? Ayo join blog kita sekarang!</p>
+                <p>UNDIPedia adalah sebuah blog dimana kamu dapat mencari dan saling berbagi mengenai informasi unik, terbaru, terhangat dan juga informatif mengenai semua hal yang terjadi di sekitar Undip. Banyak artikel-artikel yang membantu dan juga bermanfaat, mulai dari tips bagi mahasiswa yang merantau, tips olahraga, mengenal lingkungan di sekitar Undip, serta mencari informasi mengenai lomba. Kamu dapat menjadi pengunjung, maupun penulis dalam blog ini. Tunggu apalagi? Ayo join blog kita sekarang!</p>
             </div>
             </form>
         </div>
