56d4d6bb9d403fc5a10453f3a539673c295ef574
YusufSatriaBorneo <74821197+YusufSatriaBorneo@users.noreply.github.com>
2021-10-16 19:15:41 +0700

    Update db_login.php

--- admin/db_login.php
+++ admin/db_login.php
@@ -7,7 +7,7 @@ $db_password = '';
 //connect database
 $db = new mysqli($db_host, $db_username, $db_password, $db_database);
 if ($db->connect_errno) {
-	die("Could not connect to the database: <br/>" . $db->connect_error);
+	("Could not connect to the database: <br/>" . $db->connect_error);
 }
 function test_input($data)
 {
