c979df1c78915754181026f2477994431bd78e4c
mrisqiamiruladieb <92592932+mrisqiamiruladieb@users.noreply.github.com>
2021-10-16 16:41:21 +0700

    Update sign-up.php

--- sign-up.php
+++ sign-up.php
@@ -74,33 +74,33 @@ if (isset($_POST["submit"])){
 <form method="POST" autocomplete="on" action="">
  <div class="form-group">
 	<label for="name">Nama :</label>
-	<input type="text" class="form-control" id="name" name="name" value="<?php if (isset($name)) {echo $name;}?>">
-	<div class="error"><?php if (isset($error_name)) echo $error_name;?></div>
+	<input type="text" class="form-control" id="name" name="name" value="<?php if (isset($name)) {return $name;}?>">
+	<div class="error"><?php if (isset($error_name)) return $error_name;?></div>
  </div>
  <div class="form-group">
 	<label for="name">Password :</label>
 	<input type="password" class="form-control" id="password" name="password" size="30" value="">
-	<div class="error"><?php if (isset($error_password)) echo $error_password;?></div>
+	<div class="error"><?php if (isset($error_password)) return $error_password;?></div>
  </div>
  <div class="form-group">
 	<label for="name">Alamat :</label>
-	<input type="text" class="form-control" id="alamat" name="alamat" value="<?php if (isset($alamat)) {echo $alamat;}?>">
-	<div class="error"><?php if (isset($error_alamat)) echo $error_alamat;?></div>
+	<input type="text" class="form-control" id="alamat" name="alamat" value="<?php if (isset($alamat)) {return $alamat;}?>">
+	<div class="error"><?php if (isset($error_alamat)) return $error_alamat;?></div>
  </div>
  <div class="form-group">
 	<label for="name">Kota :</label>
-	<input type="text" class="form-control" id="kota" name="kota" value="<?php if (isset($kota)) {echo $kota;}?>">
-	<div class="error"><?php if (isset($error_kota)) echo $error_kota;?></div>
+	<input type="text" class="form-control" id="kota" name="kota" value="<?php if (isset($kota)) {return $kota;}?>">
+	<div class="error"><?php if (isset($error_kota)) return $error_kota;?></div>
  </div>
  <div class="form-group">
 	<label for="name">Email :</label>
-	<input type="text" class="form-control" id="email" name="email" value="<?php if (isset($email)) {echo $email;}?>">
-	<div class="error"><?php if (isset($error_email)) echo $error_email;?></div>
+	<input type="text" class="form-control" id="email" name="email" value="<?php if (isset($email)) {return $email;}?>">
+	<div class="error"><?php if (isset($error_email)) return $error_email;?></div>
  </div>
  <div class="form-group">
 	<label for="name">No. Telp :</label>
-	<input type="text" class="form-control" id="telp" name="telp" value="<?php if (isset($telp)) {echo $telp;}?>">
-	<div class="error"><?php if (isset($error_telp)) echo $error_telp;?></div>
+	<input type="text" class="form-control" id="telp" name="telp" value="<?php if (isset($telp)) {return $telp;}?>">
+	<div class="error"><?php if (isset($error_telp)) return $error_telp;?></div>
  </div>
  <br>
  <button type="submit" class="btn btn-primary" name="submit" value="submit">Submit</button>
