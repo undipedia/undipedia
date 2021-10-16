e4dfded5cdda844b5a9b921bc1b3cf31d6ee6d1e
mrisqiamiruladieb <92592932+mrisqiamiruladieb@users.noreply.github.com>
2021-10-16 17:21:59 +0700

    Update view_image.php

--- view_image.php
+++ view_image.php
@@ -1,14 +1,16 @@
 <?php
   include 'db_login.php';
 
-  if(isset($_GET['id'])) {
-	$idpost = $_GET['id'];
+  if(isset(filter_input(INPUT_GET, 'id'))) {
+	$idpost = filter_input(INPUT_GET, 'id');
     $qimg = "SELECT * FROM post WHERE idpost=" . $idpost;
-	$result = mysqli_query($db, $qimg) or die("<b>Error:</b> Problem on Retrieving Image BLOB<br/>" . mysqli_error($conn));
+	$result = mysqli_query($db, $qimg) or ("<b>Error:</b> Problem on Retrieving Image BLOB<br/>" . mysqli_error($conn));
 	$rimg = mysqli_fetch_array($result);
 	header("Content-type: image/jpeg");
-    echo $rimg['file_gambar'];
+    return $rimg['file_gambar'];
   }
   
   mysqli_close($db);
 ?>
+dari ini = $id = (isset($_POST['id']) ? $_POST['id'] : '');
+jadi ini = $id = (isset(filter_input(INPUT_POST, 'id')) ? filter_input(INPUT_POST, 'id') : '');
