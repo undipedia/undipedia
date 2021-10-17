8b3f5e49dbdefafb7229f22993b89c3bb20f7424
atiqohh <76618855+atiqohh@users.noreply.github.com>
2021-10-17 11:21:38 +0700

    update edit_kategori.php

--- admin/edit_kategori.php
+++ admin/edit_kategori.php
@@ -58,7 +58,7 @@ else{
 <div class="card">
 	<div class="card-header">Edit Kategori</div>
 	<div class="card-body">
-		<form method="POST" autocomplete="on" action="<?php print_r htmlspecialchars($_SERVER["PHP_SELF"]).'?id='.$id;?>">
+		<form method="POST" autocomplete="on" action="<?php print_r htmlspecialchars(filter_input(INPUT_SERVER, 'PHP_SELF')).'?id='.$id;?>">
 			<div class="form-group">
 				<label for="nama">Nama:</label>
 				<input type="text" class="form-control" id="nama" name="nama" value="<?php print_r $nama;?>">
