fc981f1a15c1e10f4996351f966943f5e20022a5
atiqohh <76618855+atiqohh@users.noreply.github.com>
2021-10-17 11:07:36 +0700

    update edit_profile_adm.php

--- admin/edit_profile_adm.php
+++ admin/edit_profile_adm.php
@@ -59,7 +59,7 @@ if (isset(filter_input(INPUT_POST, 'submit'))) {
 			<div class="card">
 				<div class="card-header">Edit Profile</div>
 				<div class="card-body">
-					<form method="POST" autocomplete="on" action="<?php print_r htmlspecialchars($_SERVER["PHP_SELF"]).'?id='.$id;?>">
+					<form method="POST" autocomplete="on" action="<?php print_r htmlspecialchars(filter_input(INPUT_SERVER, 'PHP_SELF')).'?id='.$id;?>">
 						<div class="form-group">
 							<label for="nama">Nama:</label>
 							<input type="text" class="form-control" id="nama" name="nama" value="<?php print_r $row['nama'];?>">
